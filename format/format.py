04038ed1ac562d8bc43a8cf363869181c9b139f0
SymphonyNo9 <kaihu@myemotibot.com>
2016-08-12 20:28:52 +0800

    change xlst

--- format/format.py
+++ format/format.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 import csv
 import sys
+import xlrd
 
 
-def format(file_name, col_index):
-    col1 = col_index[0] - 1
-    col2 = col_index[1] - 1
-    col3 = col_index[2] - 1
+def format_csv(file_name, col_index):
+    col1 = int(col_index[0]) - 1
+    col2 = int(col_index[1]) - 1
+    col3 = int(col_index[2]) - 1
     f = open(file_name, 'rb')
     wf = open('format-' + file_name, 'wb')
     csvreader = csv.reader(f, delimiter=' ')
@@ -30,13 +31,34 @@ def format(file_name, col_index):
     wf.close()
 
 
+def get_value(sh, row, col):
+    return sh.cell(row, col).value
+
+def format_xlsx(file_name, sheet_name, col_index):
+    wb = xlrd.open_workbook(file_name)
+    # 检查表单名字：
+    # print wb.sheet_names()
+    col1 = int(col_index[0]) - 1
+    col2 = int(col_index[1]) - 1
+    col3 = int(col_index[2]) - 1
+    sh = wb.sheet_by_name(sheet_name)
+    #递归打印出每行的信息：
+    for rownum in range(sh.nrows):
+        print sh.row_values(rownum)
+        if get_value(sh,rownum, col1) + get_value(sh,rownum, col2) == get_value(sh,rownum, col3):
+            pass
+        else:
+            if not get_value(sh,rownum, col2):
+                pass
+
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print 'Usage:%s %s %s \n' % (sys.argv[0], 'file name', 'column numer')
-        print 'give column numbers like 1,2,3, otherwise I will use 1,2,3 as default'
-        exit()
-    if len(sys.argv) == 3:
-        col_index = sys.argv[2].split(',')
-    else:
-        col_index = [1, 2, 3]
-    format(sys.argv[1], col_index)
+    # if len(sys.argv) < 2:
+    #     print 'Usage:%s %s %s \n' % (sys.argv[0], 'file name', 'column numer')
+    #     print 'give column numbers like 1,2,3, otherwise I will use 1,2,3 as default'
+    #     exit()
+    # if len(sys.argv) == 3:
+    #     col_index = sys.argv[2].split(',')
+    # else:
+    #     col_index = [1, 2, 3]
+    # format(sys.argv[1], col_index)
+    format_xlsx('test.xlsx', 'Sheet1', [1, 2, 3])
